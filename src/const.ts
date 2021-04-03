7319b1737f04841c2d6ae6023744291c59ea5e4b
Liam Cho <hongman.cho@gmail.com>
2021-04-04 01:43:10 +0900

    new user

--- src/const.ts
+++ src/const.ts
@@ -4,14 +4,14 @@ export const APP_ID = "26754299-6CB0-4EBE-827C-7864982B1F4D";
 // export const APP_ID = "23F5F482-A4EA-4E0C-8006-4ADA96F4566A";
 
 // set your own USER_ID and NICKNAME
-export const USER_ID = "r_Nfye7FQAGB5lHkL7d6MQ";
+export const USER_ID = "tester_062553d57ed340f5b1751e10df54ec0f"; // r_Nfye7FQAGB5lHkL7d6MQ
 export const NICKNAME = "liamchoo";
 
 export const STRIPE_APP_ID = 'pk_test_51IZ4ZnBlsD8p2lnStNhFRWIF3pgxb1mm6mXFU3czGY6i42F85FhvoJRZivxVJ6QuWRr2Y1R05nCkNtfoCnILbuOd00vZcxjAlX';
 
 export const API_HOST = 'https://api-test-jae.sendbirdtest.com';
 export const WS_HOST = 'wss://ws-test-jae.sendbirdtest.com';
-export const ACCESS_TOKEN = 'f68367bdc7a37c15f9a2c68e9e7591dc434cfaa0';
+export const ACCESS_TOKEN = '75f715760a3bc5f2cea5ffe1471b033a331597ef'; // f68367bdc7a37c15f9a2c68e9e7591dc434cfaa0
 
 export default {
   APP_ID,
